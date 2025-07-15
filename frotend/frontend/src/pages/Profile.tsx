0ed46f87e36582c0abe0e17d1d46b12afb6f2024
SakuraSher <sakurasherlocked@gmail.com>
2025-07-15 15:20:34 +0530

    modified comment to trigger frontend build

--- frotend/frontend/src/pages/Profile.tsx
+++ frotend/frontend/src/pages/Profile.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import ProtectedDataDisplay from '../components/ProtectedDataDisplay';
 
 export default function Profile() {
-  const { user,  } = useAuth0(); //removed logout function 
+  const { user,  } = useAuth0(); //removed logout function as it was not used
 
   return (
     <div>
